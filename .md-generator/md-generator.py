471d4a3593f23b5ab361414b2c9d91a25e91ba48
Quique Ciria <quiqueciria@gmail.com>
2020-04-10 08:40:04 +0200

    Update md-generator.py

--- .md-generator/md-generator.py
+++ .md-generator/md-generator.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     files = os.listdir(path)
     root = os.path.basename(path)
     dictionary = {}
-    file_content = "# Mistica icons" + BREAK + "This repo contains all icons that is working in [Mistica Design Libraries](https://github.com/Telefonica/mistica-design-libraries) now." + BREAK + "#Documentation" + BREAK + "You can find more information and how to use this repo in the [Wiki](https://github.com/Telefonica/mistica-icons/wiki)" + BREAK + "#Search in the repo" + BREAK + "The best way to search in this repo is using your browser's search engine" + BREAK + "| global | name | SVG | PDF | | O2 | name | SVG | PDF |" + BREAK + "| :-: | :- | :-: | :-: | - | :-: | :- | :-: | :-: |" + BREAK
+    file_content = "# Mistica icons" + BREAK + "This repo contains all icons that is working in [Mistica Design Libraries](https://github.com/Telefonica/mistica-design-libraries) now." + BREAK + "# Documentation" + BREAK + "You can find more information and how to use this repo in the [Wiki](https://github.com/Telefonica/mistica-icons/wiki)" + BREAK + "#Search in the repo" + BREAK + "The best way to search in this repo is using your browser's search engine" + BREAK + "| global | name | SVG | PDF | | O2 | name | SVG | PDF |" + BREAK + "| :-: | :- | :-: | :-: | - | :-: | :- | :-: | :-: |" + BREAK
     for brand in files:
         brand_folder = path + SLASH + brand
         if os.path.isdir(brand_folder) and brand != ".DS_Store":
