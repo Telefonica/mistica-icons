3579acec1cd2ad2fd8c6b01fee051ac26c76592b
Yayo <yceballost@gmail.com>
2020-04-25 15:34:56 +0200

    Create icons-tags.js

---
+++ icn_export/icons-tags.js
@@ -0,0 +1,11 @@
+// Don't touch this!
+var icnTags = [];
+///////////////////
+
+// EXAMPLE
+icnTags.lock = ["lock", "candado", "seguridad"];
+icnTags.notifications = ["notification", "notificacion", "mensaje"];
+
+
+// TEMPLATE
+icnTags.iconname = ["tag1", "tag2", "tag3", "..."];
