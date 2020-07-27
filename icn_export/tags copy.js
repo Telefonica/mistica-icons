67cb7fe6b485024110b8ff426903ba6de6287267
Yayo <yceballost@gmail.com>
2020-07-27 09:33:32 +0200

    Create tags copy.js

---
+++ icn_export/tags copy.js
@@ -0,0 +1,179 @@
+//////////////////////////////////////////////////////////////////////////////
+//// Don't touch this!                                                    ////
+var icnTags = [];
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+////                                                                      ////
+//// TEMPLATE                                                             ////
+//// icnTags.iconname = ["tag1", "tag2", "tag3", "..."];                  ////
+////                                                                      ////
+//// ———————————————————————————————————————————————————————————————————— ////
+////                                                                      ////
+//// EXAMPLE                                                              ////
+//// icnTags.lock = ["lock", "candado", "seguridad"];                     ////
+//// icnTags.notifications = ["notification", "notificacion", "mensaje"]; ////
+////                                                                      ////
+//////////////////////////////////////////////////////////////////////////////
+
+
+// PROFILE
+icnTags.lock = ["candado", "seguridad", "security"];
+icnTags.notifications = ["notificacion", "aviso", "alerta"];
+icnTags.phonebooks = ["agenda", "telefono"];
+icnTags.profile = ["perfil", "tag2"];
+icnTags.creditcard = ["tarjeta", "credito", "tag2"];
+icnTags.community = ["comunidad", "tag2"];
+icnTags.widget = ["tag1", "tag2"];
+icnTags.shop = ["tienda", "tag2"];
+icnTags.help = ["ayuda", "tag2"];
+icnTags.pin = ["codigo", "tag2"];
+icnTags.settings_invitations = ["ajustes", "invitacion"];
+icnTags.settings_support_chat = ["ajustes", "conversacion"];
+icnTags.priority = ["tag1", "tag2"];
+icnTags.priority_services = ["servicios", "tag2"];
+icnTags.settings_rating = ["ajustes", "tag2"];
+icnTags.settings_tokens = ["ajustes", "tag2"];
+icnTags.device_setup = ["dispositivo", "tag2"];
+icnTags.settings_device_support = ["dispositivo", "soporte"];
+icnTags.settings_NT = ["ajustes", "tag2"];
+icnTags.settings_errorlog = ["ajustes", "error"];
+icnTags.settings_voicemail = ["ajustes", "buzon", "voz"];
+icnTags.order = ["tag1", "tag2"];
+icnTags.bell = ["campana", "aviso", "notificacion"];
+icnTags.faq = ["faq"];
+icnTags.settings_logout = ["ajustes", "cerrar", "sesion"];
+icnTags.contact = ["contacto"];
+icnTags.request = ["peticion"];
+icnTags.protocols = ["protocolo"];
+
+// ACCOUNT
+icnTags.internet = ["internet", "red", "fibra"];
+icnTags.tv = ["television", "tele"];
+icnTags.landline = ["tag1", "tag2"];
+icnTags.mobile = ["movil", "telefono"];
+icnTags.cloud = ["nube", "tag2"];
+icnTags.nubico = ["nube", "tag2"];
+icnTags.yavoy = ["tag1", "tag2"];
+icnTags.protect = ["proteger", "tag2"];
+icnTags.data = ["datos", "tag2"];
+icnTags.premium = ["tag1", "tag2"];
+icnTags.tv_family = ["television", "tele"];
+icnTags.tv_guide = ["television", "tele"];
+icnTags.recomendation_tv = ["television", "tele"];
+icnTags.tv_cinema = ["television", "tele", "cine"];
+icnTags.tv_motor = ["television", "tele"];
+// icnTags.tv-series = ["television", "tele"];
+icnTags.tv_promo = ["television", "tele"];
+icnTags.tv_sports = ["television", "tele"];
+icnTags.tv_extra_latino = ["television", "tele"];
+icnTags.mov_balance = ["tag1", "tag2"];
+icnTags.bundle_transactions = ["paquete", "transaccion"];
+icnTags.topup_creditcard = ["recarga", "tarjeta", "credito"];
+icnTags.gift = ["regalo", "premio"];
+icnTags.topup_invoice = ["recarga", "factura"];
+icnTags.topup_balance_sos = ["recarga", "balance"];
+icnTags.others = ["otros", "miscelanea"];
+icnTags.speed_test = ["velocidad", "prueba"];
+icnTags.multi_device = ["dispositivo"];
+icnTags.wifi = ["internet", "fibra"];
+icnTags.wifi_plus = ["internet", "fibra"];
+icnTags.mobile_support = ["movil", "telefono"];
+icnTags.all_positive = ["tag1", "tag2"];
+icnTags.amount_positive = ["cantidad"];
+icnTags.amount_negative = ["cantidad"];
+icnTags.all_positive = ["tag1", "tag2"];
+icnTags.all_negative = ["tag1", "tag2"];
+icnTags.balance_positive = ["tag1", "tag2"];
+icnTags.balance_negative = ["tag1", "tag2"];
+icnTags.credit_card_positive = ["tarjeta", "credito", "pago"];
+icnTags.credit_card_negative = ["tarjeta", "credito", "pago"];
+icnTags.data_positive = ["datos", "positivo"];
+icnTags.data_negative = ["datos", "negativo"];
+icnTags.landline_positive = ["tag1", "tag2"];
+icnTags.landline_negative = ["tag1", "tag2"];
+icnTags.others_positive = ["tag1", "tag2"];
+icnTags.others_negative = ["tag1", "tag2"];
+icnTags.sms_positive = ["mensaje", "positivo"];
+icnTags.sms_negative = ["mensaje", "negativo"];
+
+// SETTINGS
+icnTags.touchid = ["huella", "seguridad"];
+icnTags.faceid = ["facial", "reconocimiento", "seguridad"];
+icnTags.delete_pin = ["eliminar", "codigo"];
+icnTags.info = ["informacion"];
+icnTags.smartphone = ["telefono", "movil"];
+icnTags.computer = ["ordenador", "pc"];
+icnTags.assistant_balance = ["asistente", "balance"];
+icnTags.assistant_bill = ["asistente", "factura"];
+icnTags.assistant_bundle = ["asistente", "paquete"];
+icnTags.assistant_data = ["asistente", "datos"];
+icnTags.assistant_device_plan = ["asistente", "dispositivo"];
+icnTags.assistant_internet = ["asistente", "fibra"];
+icnTags.assistant_issue = ["asistente", "problema", "error"];
+icnTags.assitant_landline = ["asistente", "tag2"];
+icnTags.assitant_minutes = ["asistente", "minutos"];
+icnTags.assistant_mobile = ["asistente", "telefono", "movil"];
+icnTags.assistant_notification = ["asistente", "alerta", "notificacion"];
+icnTags.assistant_sms = ["asistente", "mensaje"];
+icnTags.assistant_tv = ["asistente", "television", "tele"];
+
+// LOGIN
+icnTags.eye_disabled = ["ojo", "contraseña", "password"];
+icnTags.eye_enabled = ["ojo", "contraseña", "password"];
+
+// SUBO
+// icn-submotiv-activation-cyan
+// icn-submotiv-bars-purple
+// icn-submotiv-bill
+// icn-submotiv-cancel-green
+// icn-submotiv-combo-magenta
+// icn-submotiv-device
+// icn-submotiv-dollar
+// icn-submotiv-fiber
+// submotiv-generic
+// icn-submotiv-info
+// icn-submotiv-movistar
+// icn-submotiv-offer-tan
+// icn-submotiv-search
+// icn-submotiv-service
+// icn-submotiv-technical
+// icn-submotiv-vd-blue
+// icn-submotiv-sim-black
+// icn-submotiv-problemas-con-la-linea
+// icn-submotiv-desvio-de-llamadas
+// icn-submotiv-contestador
+// icn-submotiv-rechazo-de-llamadas
+// icn-submotiv-otros-servicios-de-llamadas
+// icn-submotiv-datos-moviles
+// icn-submotiv-estoy-en-el-extranjero
+// icn-submotiv-conexion-segura
+// icn-submotiv-aplicaciones-moviles
+// icn-submotiv-otros
+// icn-submotiv-mensajeria
+// icn-submotiv-cobertura
+// icn-submotiv-emocion
+// icn-submotiv-configuracion-wifi
+// icn-submotiv-smart-wifi
+// icn-submotiv-problemas-con-el-antivirus
+// icn-submotiv-configuracion-del-correo
+// icn-submotiv-canales
+// icn-submotiv-descodificador-y-mando
+// icn-submotiv-configuracion-de-opciones
+// icn-submotiv-series-peliculas-y-otros
+// icn-submotiv-ultimos-7-dias-y-directo
+icnTags.motiv_fijo = ["tag1", "tag2"];
+icnTags.motiv_internet = ["tag1", "tag2"];
+icnTags.motiv_tv = ["tag1", "tag2"];
+icnTags.ticket_OB = ["tag1", "tag2"];
+icnTags.support_chat = ["tag1", "tag2"];
+icnTags.support_tickets = ["tag1", "tag2"];
+icnTags.contact_agent = ["tag1", "tag2"];
+icnTags.ticket_closed = ["tiquet", "billete"];
+icnTags.ticket_received = ["tiquet", "billete"];
+icnTags.ticket_standby = ["tiquet", "billete"];
+icnTags.ticket_technical = ["tiquet", "billete"];
+icnTags.ticket_pending = ["tiquet", "billete"];
+icnTags.service_credit_extension = ["servicio", "tag2"];
+icnTags.service_landline_invoice = ["servicio", "tag2"];
+icnTags.service_exchange_plan = ["servicio", "tag2"];
