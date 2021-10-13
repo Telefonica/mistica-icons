1ba918bd4bffa26d8b51e9ac0f5aac7a9dcde464
Yayo <yceballost@gmail.com>
2021-10-13 17:35:58 +0200

    refactor md-generator

--- .github/md-generator/md-generator.py
+++ .github/md-generator/md-generator.py
@@ -5,47 +5,61 @@ import sys
 
 PIPE = "|"
 SLASH = "/"
-PNG_FOLDER = "PNG"
-SVG_FOLDER = "SVG"
-PDF_FOLDER = "PDF"
-PNG_EXTENSION = ".png"
 SVG_EXTENSION = ".svg"
 PDF_EXTENSION = ".pdf"
 BREAK = "\n"
 
+
+def read_folder(folder):
+    if os.path.isdir(folder):
+        files = os.listdir(folder)
+        if ".DS_Store" in files:
+            files.remove(".DS_Store")
+        return files
+    return []
+
+
 if __name__ == '__main__':
     path = sys.argv[1]
-    files = os.listdir(path)
+    brands = read_folder(path)
     root = os.path.basename(path)
     dictionary = {}
-    file_content = "<br/><br/>![Mistica Icons](.github/resources/misticaicons-logo.png)<br/><br/>" + BREAK + "### What is this?" + BREAK + "This is the repo that contains all icons that is working in [Mistica Design](https://github.com/Telefonica/mistica-design) now." + BREAK + "Mistica support [Brand Factory icons](https://brandfactory.telefonica.com/document/1086#/nuestra-identidad/iconos). This set of icons are a big list of different icons and style that Brand Team worked to be used through Telefonica applications." + BREAK + "If you have any question, please you can ask directly in the app of Microsoft Teams, in [Mistica Team](https://teams.microsoft.com/l/team/19%3ad2e3607a32ec411b8bf492f43cd0fe0c%40thread.tacv2/conversations?groupId=e265fe99-929f-45d1-8154-699649674a40&tenantId=9744600e-3e04-492e-baa1-25ec245c6f10)." + BREAK + "### Documentation" + BREAK + "#### Develop" + BREAK + "##### iOS and Android" + BREAK + "You can get .pdf or .svg files from this repo." + BREAK + "##### Web" + BREAK + "Visit [Mistica Storybook](https://mistica-web.now.sh/?path=/story/icons-mistica-icons--catalog) to get all the detail about using Mistica Icons Library" + BREAK + "#### Design" + BREAK + "Install Mistica Icons Library in Sketch from [Mistica Manager](https://telefonica.github.io/mistica/docs/design/start-using)" + BREAK + "# Icons " + BREAK + "| Icon | name 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 | SVG | PDF |" + BREAK + "| :-: | :- | :-: | :-: |" + BREAK
-    for brand in files:
+    file_content = "<br/><br/>![Mistica Icons](.github/resources/misticaicons-logo.png)<br/><br/>" + BREAK + "### What is this?" + BREAK + "This is the repo that contains all icons that is working in [Mistica Design](https://github.com/Telefonica/mistica-design) now." + BREAK + "Mistica support [Brand Factory icons](https://brandfactory.telefonica.com/document/1086#/nuestra-identidad/iconos). This set of icons are a big list of different icons and style that Brand Team worked to be used through Telefonica applications." + BREAK + "If you have any question, please you can ask directly in the app of Microsoft Teams, in [Mistica Team](https://teams.microsoft.com/l/team/19%3ad2e3607a32ec411b8bf492f43cd0fe0c%40thread.tacv2/conversations?groupId=e265fe99-929f-45d1-8154-699649674a40&tenantId=9744600e-3e04-492e-baa1-25ec245c6f10)." + BREAK + "### Documentation" + BREAK + "#### Develop" + BREAK + "##### iOS and Android" + BREAK + "You can get .pdf or .svg files from this repo." + BREAK + "##### Web" + BREAK + "Visit [Mistica Storybook](https://mistica-web.now.sh/?path=/story/icons-mistica-icons--catalog) to get all the detail about using Mistica Icons Library" + BREAK + "#### Design" + BREAK + "Install Mistica Icons Library in Sketch from [Mistica Manager](https://telefonica.github.io/mistica/docs/design/start-using)" + BREAK + "# Icons " + BREAK + "| ---BRANDS--- | name  | " + BREAK + "| ---HEADER-BREAK--- |" + BREAK
+    for brand in brands:
         brand_folder = path + SLASH + brand
-        if os.path.isdir(brand_folder) and brand != ".DS_Store":
-            for icon in os.listdir(brand_folder):
-                if icon != ".DS_Store":
-                    icon_name = os.path.splitext(icon)[0]
-                    icon_extension = os.path.splitext(icon)[1]
-                    if icon_name in dictionary:
-                        brands = dictionary[icon_name]
-                        if brand not in brands:
-                            brands.append(brand)
+        styles = read_folder(brand_folder)
+        for style in styles:
+            style_folder = brand_folder + SLASH + style
+            icons = read_folder(style_folder)
+            for icon in icons:
+                icon_name = os.path.splitext(icon)[0]
+                file_path = root + SLASH + brand + SLASH + style + SLASH + icon_name + SVG_EXTENSION
+                if icon_name in dictionary:
+                    if style not in dictionary[icon_name]:
+                        dictionary[icon_name][style] = {brand: file_path}
                     else:
-                        dictionary[icon_name] = [brand]
+                        dictionary[icon_name][style][brand] = file_path
+                else:
+                    dictionary[icon_name] = {
+                        style: {brand: file_path}
+                    }
 
-    for icon in sorted(dictionary.keys()):
-        line_image = PIPE
-        line_separator = BREAK + PIPE
-        line_name = BREAK + PIPE
-        row = ""
-        counter = 0
-        dictionary[icon].sort()
-        for brand in dictionary[icon]:
-            path_icon = root + SLASH + brand + SLASH + icon + SVG_EXTENSION
-            svg_icon = root + SLASH + brand + SLASH + icon + SVG_EXTENSION
-            pdf_icon = root + SLASH + brand + SLASH + icon + PDF_EXTENSION
-            row = row + "| ![" + icon + "](" + path_icon + ") | `" + icon + "`  |  [.svg](" + svg_icon + ") | [.pdf](" + pdf_icon + ") |  "
-        file_content += row + BREAK
+    brands.remove("default")
+    brands = ["default"] + sorted(brands)
+    separator = " " + PIPE + " "
+    file_content = file_content.replace("---BRANDS---", separator.join(brands))
+    file_content = file_content.replace("---HEADER-BREAK---", separator.join(["---"] * (len(brands) + 1)))
+
+    for icon_name in sorted(dictionary.keys()):
+        icon = dictionary[icon_name]
+        for style in sorted(icon.keys()):
+            icon_images = []
+            for brand in brands:
+                icon_image = " ![" + icon_name + "](" + icon[style][brand] + ") " if brand in icon[style] else " "
+                icon_images.append(icon_image)
+            # row = "| default | O2 | my_icon_light |
+            row = PIPE + PIPE.join(icon_images) + PIPE + icon_name + PIPE
+            file_content += row + BREAK
 
     output_file_path = "./README.md"
     print(output_file_path)
