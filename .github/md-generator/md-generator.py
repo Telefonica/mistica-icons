f1670a33a4037e7972b216c600e27fce95c03f03
Yayo <yceballost@gmail.com>
2022-07-30 01:46:20 +0200

    update position

--- .github/md-generator/md-generator.py
+++ .github/md-generator/md-generator.py
@@ -98,11 +98,11 @@ blau_global_bar = (int(blau_global_percentage / 10) * 2) * BAR_FILLED + \
     BAR_EMPTY * (abs(int(blau_global_percentage / 10) - 10) * 2)
 
 # Composition of GLOBAL BAR + (X / Y) for markdown
-telefonica_global = ("Telefónica set" + "<br/>" + telefonica_global_bar + "    " +
+telefonica_global = (telefonica_global_bar + "    " +
                      str(telefonica_global_percentage) + "%" + comp_global_telefonica + " `global`" + "  ")
-o2_global = ("O<sub>2</sub> set" + "<br/>" + o2_global_bar + "    " +
+o2_global = (o2_global_bar + "    " +
              str(o2_global_percentage) + "%" + comp_global_o2 + " `global`" + "  ")
-blau_global = ("Blau set" + "<br/>" + blau_global_bar + "    " +
+blau_global = (blau_global_bar + "    " +
                str(blau_global_percentage) + "%" + comp_global_blau + " `global`" + "  ")
 
 
@@ -147,11 +147,11 @@ comp_local_blau = " " + \
     " / " + str(len(icons_blau)) + ")"
 
 # Composition of LOCAL BAR + (X / Y) for markdown
-telefonica_local = (telefonica_local_bar + "    " +
+telefonica_local = ("Telefónica set" + "<br/>" + telefonica_local_bar + "    " +
                     str(telefonica_local_percentage) + "%" + comp_local_telefonica + " `local`" + "  ")
-o2_local = (o2_local_bar + "    " +
+o2_local = ("O<sub>2</sub> set" + "<br/>" + o2_local_bar + "    " +
             str(o2_local_percentage) + "%" + comp_local_o2 + " `local`" + "  ")
-blau_local = (blau_local_bar + "    " +
+blau_local = ("Blau set" + "<br/>" + blau_local_bar + "    " +
               str(blau_local_percentage) + "%" + comp_local_blau + " `local`" + "  ")
 
 
@@ -166,7 +166,7 @@ if __name__ == '__main__':
     file_content = "# Mística Icons" + BREAK + BREAK + "![Mistica Icons](.github/resources/mistica-icons.svg)" + BREAK + BREAK + "## What is this?  " + BREAK + BREAK + "This is the repo that contains all icons that is working in [Mistica Design](https://github.com/Telefonica/mistica-design) now.  " + BREAK + BREAK + "Mistica support [Brand Factory icons](https://brandfactory.telefonica.com/document/1086#/nuestra-identidad/iconos). This set of icons are a big list of different icons and style that Brand Team worked to be used through Telefonica applications." + BREAK + BREAK + "If you have any question, please you can ask directly in the app of Microsoft Teams, in [Mistica Team](https://teams.microsoft.com/l/team/19%3ad2e3607a32ec411b8bf492f43cd0fe0c%40thread.tacv2/conversations?groupId=e265fe99-929f-45d1-8154-699649674a40&tenantId=9744600e-3e04-492e-baa1-25ec245c6f10).  " + \
         BREAK + BREAK + "### Documentation" + BREAK + BREAK + "#### Develop" + BREAK + BREAK + "##### iOS and Android" + BREAK + BREAK + "You can get .pdf or .svg files from this repo." + BREAK + BREAK + "##### Web" + BREAK + BREAK + \
         "Visit [Mistica Storybook](https://mistica-web.now.sh/?path=/story/icons-mistica-icons--catalog) to get all the detail about using Mistica Icons Library" + BREAK + BREAK + "#### Design" + BREAK + BREAK + "Use Mística icons library in Figma!" + BREAK + BREAK + \
-        "### Icon equivalence status" + BREAK + BREAK + "**Global** = Icon pack equivalence with total icons  " + BREAK + BREAK + "**Local** = Icon equivalence in this pack" + BREAK + BREAK + "---telefonica_global_BAR---" + BREAK + "---telefonica_local_BAR---" + BREAK + BREAK + "---O2_global_BAR---" + BREAK + "---o2_local_BAR---" + BREAK + BREAK + "---BLAU_global_BAR---" + BREAK + "---blau_local_BAR---" + BREAK + BREAK + \
+        "### Icon equivalence status" + BREAK + BREAK + "**Local** = Icon equivalence in this pack  " + BREAK + "**Global** = Icon pack equivalence with total icons" + BREAK + BREAK + "---telefonica_local_BAR---" + BREAK + "---telefonica_global_BAR---" + BREAK + BREAK + "---o2_local_BAR---" + BREAK + "---o2_global_BAR---" + BREAK + BREAK + "---blau_local_BAR---" + BREAK + "---blau_global_BAR---" + BREAK + BREAK + \
         "## Icons" + BREAK + BREAK + "| ---BRANDS--- | icon name <img width=500> |" + \
         BREAK + "| ---HEADER-BREAK--- |" + ":--- |" + BREAK
     for brand in brands:
@@ -197,11 +197,11 @@ if __name__ == '__main__':
         "---telefonica_global_BAR---", (telefonica_global))
     file_content = file_content.replace(
         "---telefonica_local_BAR---", (telefonica_local))
-    file_content = file_content.replace("---O2_global_BAR---", (o2_global))
+    file_content = file_content.replace("---o2_global_BAR---", (o2_global))
     file_content = file_content.replace(
         "---o2_local_BAR---", (o2_local))
     file_content = file_content.replace(
-        "---BLAU_global_BAR---", (blau_global))
+        "---blau_global_BAR---", (blau_global))
     file_content = file_content.replace(
         "---blau_local_BAR---", (blau_local))
     file_content = file_content.replace("---BRANDS---", separator.join(brands))
