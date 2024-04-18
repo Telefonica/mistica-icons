ac127638762d64a0531caf3c67e940079746e463
Yayo <yceballost@gmail.com>
2024-04-18 15:49:02 +0200

    use missing name

--- .github/md-generator/generate_graph.py
+++ .github/md-generator/generate_graph.py
@@ -4,7 +4,7 @@ import os
 all_equivalence_color = "0066FF"
 equivalence_color = "EAC344"
 unique_color = "59C2C9"
-remaining_color = "D1D5E4"
+missing_color = "D1D5E4"
 
 def preprocess_filename(filename):
     return filename.replace("filled", "").replace("light", "").replace("regular", "")
@@ -90,12 +90,12 @@ def generate_bar_representation(icons_folder):
         unique_width = max(1, int(unique_percentage * bar_width / 100))
         equivalence_width = max(1, int(equivalence_percentage * bar_width / 100))
         all_equivalence_width = max(1, int(all_equivalence_percentage * bar_width / 100))
-        remaining_width = max(1, bar_width - unique_width - equivalence_width - all_equivalence_width)
+        missing_width = max(1, bar_width - unique_width - equivalence_width - all_equivalence_width)
         
         bar = f"![](https://dummyimage.com/{all_equivalence_width}x8/{all_equivalence_color}/000000?text=+)" \
               f"![](https://dummyimage.com/{equivalence_width}x8/{equivalence_color}/000000?text=+)" \
               f"![](https://dummyimage.com/{unique_width}x8/{unique_color}/000000?text=+)" \
-              f"![](https://dummyimage.com/{remaining_width}x8/{remaining_color}/000000?text=+)"
+              f"![](https://dummyimage.com/{missing_width}x8/{missing_color}/000000?text=+)"
         
         bar_representation.append(bar)
 
@@ -125,14 +125,14 @@ def icons_equivalence_data_table(root_folder):
     total_icons = sum(total_counts)
 
     markdown_table = "<br/>\n"
-    markdown_table += f"\n| Icon Set | Concepts | Total | All Equivalence | Some Equivalence | Unique | Remaining |\n"
+    markdown_table += f"\n| Icon Set | Concepts | Total | All Equivalence | Some Equivalence | Unique | Missing |\n"
     markdown_table += "|:--------|-------------:|--------------:|----------:|------------------------:|---------------------------:|-------------:|\n"
     for label, folder, total_count, unique_count, equivalent_count, all_equivalent_count in zip(labels, folders, total_counts, unique_counts, equivalent_counts, all_equivalent_counts):
         unique_percent = f"{unique_count / total_icons * 100:.1f}%" + f" ![](https://dummyimage.com/8x8/{unique_color}/000000?text=+)" if total_icons > 0 else "0%"
         equivalent_percent = f"{equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{equivalence_color}/000000?text=+)"
         all_equivalent_percent = f"{all_equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{all_equivalence_color}/000000?text=+) "
-        remaining_percent = f"{100 - float(unique_count / total_icons * 100) - equivalent_count - all_equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{remaining_color}/000000?text=+) " if total_icons > 0 else "0%"
-        markdown_table += f"| {label} | {unique_count} | {total_count} | {all_equivalent_percent} | {equivalent_percent} | {unique_percent} | {remaining_percent} |\n"
+        missing_percent = f"{100 - float(unique_count / total_icons * 100) - equivalent_count - all_equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{missing_color}/000000?text=+) " if total_icons > 0 else "0%"
+        markdown_table += f"| {label} | {unique_count} | {total_count} | {all_equivalent_percent} | {equivalent_percent} | {unique_percent} | {missing_percent} |\n"
 
     markdown_table += "\n"
     markdown_table += "<sub>"
@@ -160,7 +160,7 @@ def icons_equivalence_data_table(root_folder):
     markdown_table += "</sub>  "
     markdown_table += "\n"
     markdown_table += "<sub>"
-    markdown_table += "**Remaining:** The remaining percentage of icons after taking into account unique icons, icons with some equivalence, and icons with equivalence in all sets. This percentage represents the icons that do not fall into any of these categories."
+    markdown_table += "**Missing:** The missing percentage of icons after taking into account unique icons, icons with some equivalence, and icons with equivalence in all sets. This percentage represents the icons that do not fall into any of these categories."
     markdown_table += "</sub>"
     markdown_table += "\n\n"
 
