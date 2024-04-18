cc937b9a5195fda723d40819a694c3525dc9508a
Yayo <yceballost@gmail.com>
2024-04-18 20:46:50 +0200

    update

--- .github/md-generator/generate_graph.py
+++ .github/md-generator/generate_graph.py
@@ -1,5 +1,8 @@
 import os
 
+# Define the desired order of folders
+brands = ["telefonica", "o2", "vivo-new", "blau"]
+
 # Define los colores una vez fuera de las funciones
 all_equivalence_color = "0066FF"
 equivalence_color = "EAC344"
@@ -41,7 +44,7 @@ def count_equivalent_icons(folder, all_folders):
                         if other_folder != folder:
                             other_subfolder_path = os.path.join(other_folder, subfolder)
                             if os.path.isdir(other_subfolder_path):
-                                if preprocess_filename(filename) in [preprocess_filename(f) for f in os.listdir(other_subfolder_path) if not f.startswith('.')]:
+                                if preprocess_filename(filename) in [preprocess_filename(f) for f in os.listdir(other_subfolder_path) if not f.startswith('.')]: 
                                     equivalent += 1
                                     break
     return equivalent / total_icons * 100 if total_icons > 0 else 0
@@ -65,37 +68,55 @@ def count_all_equivalent_icons(folder, all_folders):
                         all_equivalent += 1
     return all_equivalent / total_icons * 100 if total_icons > 0 else 0
 
+def count_missing_icons(folder, all_folders):
+    missing = 0
+    total_icons = count_total_icons(folder)
+    for subfolder in os.listdir(folder):
+        subfolder_path = os.path.join(folder, subfolder)
+        if os.path.isdir(subfolder_path):
+            for filename in os.listdir(subfolder_path):
+                if filename.endswith(".svg") and not filename.startswith('.'):
+                    found_in_all = False
+                    for other_folder in all_folders:
+                        other_subfolder_path = os.path.join(other_folder, subfolder)
+                        if os.path.isdir(other_subfolder_path):
+                            if preprocess_filename(filename) in [preprocess_filename(f) for f in os.listdir(other_subfolder_path) if not f.startswith('.')]:
+                                found_in_all = True
+                                break
+                    if not found_in_all:
+                        missing += 1
+    return missing
+
 def generate_bar_representation(icons_folder):
     bar_width = 500
     
-    # Define the desired order of folders
-    desired_order = ["telefonica", "o2", "vivo-new", "blau"]  # Add more folders as needed
-    
     # Create full paths for the folders in the desired order
-    folders = [os.path.join(icons_folder, folder) for folder in desired_order]
+    folders = [os.path.join(icons_folder, folder) for folder in brands]
     
     # Extract labels from folder names
-    labels = desired_order
+    labels = brands
     
     total_counts = [count_total_icons(folder) for folder in folders]
     unique_counts = [count_unique_icons(folder) for folder in folders]
     equivalence_percentages = [count_equivalent_icons(folder, folders) for folder in folders]
     all_equivalence_percentages = [count_all_equivalent_icons(folder, folders) for folder in folders]
+    missing_counts = [count_missing_icons(folder, folders) for folder in folders]  # Calculate missing counts
+
     total_icons = sum(total_counts)
 
     bar_representation = []
 
-    for unique_count, equivalence_percentage, all_equivalence_percentage in zip(unique_counts, equivalence_percentages, all_equivalence_percentages):
+    for unique_count, equivalence_percentage, all_equivalence_percentage, missing_count in zip(unique_counts, equivalence_percentages, all_equivalence_percentages, missing_counts):
         unique_percentage = unique_count / total_icons * 100
         unique_width = max(1, int(unique_percentage * bar_width / 100))
         equivalence_width = max(1, int(equivalence_percentage * bar_width / 100))
         all_equivalence_width = max(1, int(all_equivalence_percentage * bar_width / 100))
-        missing_width = max(1, bar_width - unique_width - equivalence_width - all_equivalence_width)
+        missing_width = max(1, int((missing_count / total_counts[0]) * bar_width))  # Calculate missing width
         
         bar = f"![](https://dummyimage.com/{all_equivalence_width}x8/{all_equivalence_color}/000000?text=+)" \
               f"![](https://dummyimage.com/{equivalence_width}x8/{equivalence_color}/000000?text=+)" \
               f"![](https://dummyimage.com/{unique_width}x8/{unique_color}/000000?text=+)" \
-              f"![](https://dummyimage.com/{missing_width}x8/{missing_color}/000000?text=+)"
+              f"![](https://dummyimage.com/{missing_width}x8/{missing_color}/000000?text=+)"  # Add missing bar
         
         bar_representation.append(bar)
 
@@ -108,31 +129,37 @@ def generate_bar_representation(icons_folder):
 
 
 def icons_equivalence_data_table(root_folder):
-    # Define el orden deseado de las carpetas
-    desired_order = ["telefonica", "o2", "vivo-new", "blau"]  # Agrega más carpetas según sea necesario
-    
     # Crea rutas completas para las carpetas en el orden deseado
-    folders = [os.path.join(root_folder, folder) for folder in desired_order]
+    folders = [os.path.join(root_folder, folder) for folder in brands]
     
     # Extrae etiquetas de los nombres de las carpetas
-    labels = desired_order
+    labels = brands
 
     unique_counts = [count_unique_icons(folder) for folder in folders]
     equivalent_counts = [count_equivalent_icons(folder, folders) for folder in folders]
     all_equivalent_counts = [count_all_equivalent_icons(folder, folders) for folder in folders]
     total_counts = [count_total_icons(folder) for folder in folders]
+    missing_counts = [count_missing_icons(folder, folders) for folder in folders]  # Calculate missing counts
 
     total_icons = sum(total_counts)
 
     markdown_table = "<br/>\n"
     markdown_table += f"\n| Icon Set | Concepts | Total | All Equivalence | Some Equivalence | Unique | Missing |\n"
     markdown_table += "|:--------|-------------:|--------------:|----------:|------------------------:|---------------------------:|-------------:|\n"
-    for label, folder, total_count, unique_count, equivalent_count, all_equivalent_count in zip(labels, folders, total_counts, unique_counts, equivalent_counts, all_equivalent_counts):
+    for label, folder, total_count, unique_count, equivalent_count, all_equivalent_count, missing_count in zip(labels, folders, total_counts, unique_counts, equivalent_counts, all_equivalent_counts, missing_counts):
         unique_percent = f"{unique_count / total_icons * 100:.1f}%" + f" ![](https://dummyimage.com/8x8/{unique_color}/000000?text=+)" if total_icons > 0 else "0%"
         equivalent_percent = f"{equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{equivalence_color}/000000?text=+)"
         all_equivalent_percent = f"{all_equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{all_equivalence_color}/000000?text=+) "
-        missing_percent = f"{100 - float(unique_count / total_icons * 100) - equivalent_count - all_equivalent_count:.1f}%" + f" ![](https://dummyimage.com/8x8/{missing_color}/000000?text=+) " if total_icons > 0 else "0%"
+        missing_percent = f"{missing_count / total_count * 100:.1f}%" + f" ![](https://dummyimage.com/8x8/{missing_color}/000000?text=+) "  # Calculate missing percent
         markdown_table += f"| {label} | {unique_count} | {total_count} | {all_equivalent_percent} | {equivalent_percent} | {unique_percent} | {missing_percent} |\n"
+        print(f"Icon Set: {label}")
+        print(f"Total Concepts: {unique_count}")
+        print(f"Total Icons: {total_count}")
+        print(f"All Equivalence: {all_equivalent_percent}")
+        print(f"Some Equivalence: {equivalent_percent}")
+        print(f"Unique: {unique_percent}")
+        print(f"Missing: {missing_percent}")
+        print("\n")
 
     markdown_table += "\n"
     markdown_table += "<sub>"
@@ -165,3 +192,9 @@ def icons_equivalence_data_table(root_folder):
     markdown_table += "\n\n"
 
     return markdown_table
+
+root_folder = "icons"
+print("Bar Representation:")
+print(generate_bar_representation(root_folder))
+print("\nIcons Equivalence Data Table:")
+print(icons_equivalence_data_table(root_folder))
